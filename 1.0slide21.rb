946171541c8426ac40a1507ab833da6cb1a82237
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-10-27 00:18:25 +0800

    arrays and hashes

--- 1.0slide21.rb
+++ 1.0slide21.rb
@@ -31,8 +31,8 @@ contact_data = [["john@email.com", "123 Main st.", "555-123-4567"],
 
 contacts = {"John Cruz" => {}, "Avion School" => {}}
 
-contacts["John Cruz"] = { :email => contact_data[0][0], :address => contact_data[0][1], :phone => contact_data[0][2]}"
-contacts["Avion School"] = { :email => contact_data[1][0], :address => contact_data[1][1], :phone => contact_data[1][2]}"
+contacts["John Cruz"] = { :email => contact_data[0][0], :address => contact_data[0][1], :phone => contact_data[0][2]}
+contacts["Avion School"] = { :email => contact_data[1][0], :address => contact_data[1][1], :phone => contact_data[1][2]}
 
 puts "#{contacts}"
 
