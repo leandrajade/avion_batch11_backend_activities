4dca9141ef92b606c461513bf35db493087b3c22
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-10-27 01:39:04 +0800

    class inheritance, super

---
+++ 2.0slide17a.rb
@@ -0,0 +1,23 @@
+class Confection
+    def bake
+        puts "Baking at 350 degrees for 25 minutes."
+    end
+end
+
+class Cupcake < Confection
+    def bake
+        super && "Applying frosting"
+    end
+end
+
+class BananaCake < Confection; end
+
+## execute
+
+myCupcake = Cupcake.new
+myCupcake.bake
+#myCupcake.frost
+
+myBananaCake = BananaCake.new
+myBananaCake.bake
+
