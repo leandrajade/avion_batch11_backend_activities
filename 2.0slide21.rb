2429d0d1f788f3ada79b1f255e5bb43ddbec7253
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-10-27 01:44:09 +0800

    abstraction

---
+++ 2.0slide21.rb
@@ -0,0 +1,31 @@
+class Picture
+    def initialize(camera = false, subject = false, image = false)
+        @camera = camera
+        @subject = subject
+        @image = image
+    end
+
+    def take_picture
+        pose_subject #invoking a private method
+        click
+        @image = true
+    end
+
+    def click
+        use_camera #invoking a private method
+        puts "clicked the shutter button"
+    end
+
+    private
+  
+    def use_camera
+        @camera = true
+        puts "switching camera on"
+    end
+
+    def pose_subject
+        @subject = true
+        puts "posing subject"
+    end
+end
+
