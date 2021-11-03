436f948c9b69b144cbbabe0d1487909c34704156
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-03 19:17:26 +0800

    isogram

--- isograms.rb
+++ isograms.rb
@@ -29,31 +29,14 @@ word = gets.chomp
 word1 = Isograms.new(word)
 puts word1.is_isogram()
 
-# letter_array = []
+# letters_present = []
 # i = 0
 # while i < arr_string.length - 1
 #     if letter_array.includes?arr_string[i]
 #         return false 
-#         break
 #     else
 #         letter_array.push(arr_string[i])
 #     end
 #     i+1
 # end
 # return true
-
-
-
-
-# i = 0
-# while i < arr_string.length - 1
-#     current = arr_string[i]
-#     nextt = arr_string[i+1]
-#     if current == nextt
-#         puts "false"
-#         break
-#     else
-#         puts "true"
-#         i += 1
-#     end
-# end           
