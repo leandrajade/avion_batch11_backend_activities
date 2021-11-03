bba8398c5083e7ae4e0f2a8a4f691fed68d20569
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-03 13:19:47 +0800

    no repeating letters (consecutive and non-consecutive)

---
+++ isograms.rb
@@ -0,0 +1,43 @@
+class Isograms
+    def initialize(string)
+        @string = string
+    end
+
+    def to_lower_case
+        return @string.downcase
+    end
+
+    def separate_characters
+        string = self.to_lower_case()
+        return string.chars
+    end
+
+    def is_isogram
+        arr_string = self.separate_characters()
+        if arr_string.detect{ |letter| arr_string.count(letter) > 1}
+            return false
+        else
+            return true
+        end
+    end
+end
+
+## execute
+puts "enter a word to check if it is an isogram or not"
+word = gets.chomp
+
+word1 = Isograms.new(word)
+puts word1.is_isogram()
+
+# i = 0
+# while i < arr_string.length - 1
+#     current = arr_string[i]
+#     nextt = arr_string[i+1]
+#     if current == nextt
+#         puts "false"
+#         break
+#     else
+#         puts "true"
+#         i += 1
+#     end
+# end           
