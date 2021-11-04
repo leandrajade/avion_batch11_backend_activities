41d4cba30e1ecd29819f6bbbfd85e8eed7cce52e
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-04 16:12:15 +0800

    minimum number of bills and coins

--- denomination.rb
+++ denomination.rb
@@ -1,6 +1,5 @@
 # minimum bills and coins for change
 class CashRegister 
-    
     def set_total_amount(total_amount)
         @total_amount = total_amount
         return total_amount
@@ -10,11 +9,6 @@ class CashRegister
         @payment_amount = payment_amount
         return payment_amount
     end
-
-    def calculate_change
-        change = @payment_amount - @total_amount
-        return change
-    end 
     
     def display_change
         minimum_denomination = self.get_change()
@@ -23,10 +17,15 @@ class CashRegister
         end
     end
 
+    def calculate_change
+        change = @payment_amount - @total_amount
+        return change
+    end 
+
     def get_change
         change = self.calculate_change()
         currencies = [1000, 500, 200, 100, 50, 20, 10, 5, 1]
-        minimum_denomination = {} #location to set the currency and its count (currency:count)
+        minimum_denomination = {} #location to set the currency and its count (currency=>count)
 
         currencies.each do |currency| 
             if change >= currency
@@ -36,25 +35,23 @@ class CashRegister
         end
 
         return minimum_denomination
-
-        # until change == 0
-        #     if change >= 1000
-        #         @oneThousand = change / 1000
-        #         change -=@oneThousand * 1000
-        #         return @oneThousand
-        #     elsif change >= 500
-        #         @fiveHundred = change / 500
-        #         change -= @fiveHundred * 500
-        #         return @fiveHundred
-        #           ...
-        
     end
 end
 
 ## execute
-
 change = CashRegister.new()
 puts change.set_total_amount(250)
 puts change.set_payment_amount(1000)
 change.get_change()
 change.display_change()
+
+# until change == 0
+#     if change >= 1000
+#         @oneThousand = change / 1000
+#         change -=@oneThousand * 1000
+#         return @oneThousand
+#     elsif change >= 500
+#         @fiveHundred = change / 500
+#         change -= @fiveHundred * 500
+#         return @fiveHundred
+#           ...
