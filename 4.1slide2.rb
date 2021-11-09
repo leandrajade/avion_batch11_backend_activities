0915ff9802e5da62150623a11b488dd6dd934114
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-11-09 15:57:58 +0800

    calculates given year

--- 4.1slide2.rb
+++ 4.1slide2.rb
@@ -1,9 +1,3 @@
-# The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including
-# the year 200, etc.
-
-# Given a year, return the century it is in.
-
-# require 'date'
 class CenturyCalculator
     def initialize(input)
         @input = input
